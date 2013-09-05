5c2844a61f472f9645f4dfe8001b99652f90db00
Andrew Toulouse <andrew@atoulou.se>
2013-09-05 02:06:57 -0700

    Initial commit of scaffolding

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME=ATLeagueUtility
+
+[ -d "CMakeFiles" ] && rm -r "CMakeFiles"
+[ -d "CMakeScripts" ] && rm -r "CMakeScripts"
+[ -d "$NAME.build" ] && rm -r "$NAME.build"
+[ -d "$NAME.xcodeproj" ] && rm -r "$NAME.xcodeproj"
+[ -d "build" ] && rm -r "build"
+
+[ -f "CMakeCache.txt" ] && rm "CMakeCache.txt"
+[ -f "cmake_install.cmake" ] && rm "cmake_install.cmake"
+[ -f "Makefile" ] && rm "Makefile"
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <boost/archive/binary_oarchive.hpp>
+
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+}
