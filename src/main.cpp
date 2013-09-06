407b31d7191b75dc59ea816353d11d98777273ce
Andrew Toulouse <andrew@atoulou.se>
2013-09-06 00:45:00 -0700

    Add simple file loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include <boost/archive/binary_oarchive.hpp>
 
 using namespace std;
 
 int main(int argc, const char* argv[]) {
+    if (argc != 2) {
+        cerr << "Not enough arguments!" << endl;
+        return -1;
+    }
+
+    ifstream is;
+    filebuf * fb = is.rdbuf();
+    fb->open(argv[1], ios::in|ios::binary);
+
+    if (!fb->is_open()) {
+        cerr << "File couldn't be opened!" << endl;
+        return -2;
+    }
+
+    return 0;
 }
