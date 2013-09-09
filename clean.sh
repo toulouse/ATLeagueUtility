328094ca28d05939e05da0d9e2835263db89e167
Andrew Toulouse <andrew@atoulou.se>
2013-09-08 22:26:07 -0700

    Add ignore/clean for Eclipse projects

--- clean.sh
+++ clean.sh
@@ -2,12 +2,15 @@
 
 NAME=ATLeagueUtility
 
-[ -d "CMakeFiles" ] && rm -r "CMakeFiles"
-[ -d "CMakeScripts" ] && rm -r "CMakeScripts"
-[ -d "$NAME.build" ] && rm -r "$NAME.build"
-[ -d "$NAME.xcodeproj" ] && rm -r "$NAME.xcodeproj"
 [ -d "build" ] && rm -r "build"
 
+[ -d "$NAME.build" ] && rm -r "$NAME.build"
+[ -d "$NAME.xcodeproj" ] && rm -r "$NAME.xcodeproj"
+[ -f ".cproject" ] && rm ".cproject"
+[ -f ".project" ] && rm ".project"
+[ -f "Makefile" ] && rm "Makefile"
+
+[ -d "CMakeFiles" ] && rm -r "CMakeFiles"
+[ -d "CMakeScripts" ] && rm -r "CMakeScripts"
 [ -f "CMakeCache.txt" ] && rm "CMakeCache.txt"
 [ -f "cmake_install.cmake" ] && rm "cmake_install.cmake"
-[ -f "Makefile" ] && rm "Makefile"
